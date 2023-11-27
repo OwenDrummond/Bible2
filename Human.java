3ad6902c66f9e58d7f2e79c172d2a9a1bb1a5f48
OwenDrummond <129247532+OwenDrummond@users.noreply.github.com>
2023-11-26 21:21:39 -0600

    Update Human.java

--- Human.java
+++ Human.java
@@ -2,8 +2,8 @@
 public class Human
 {
   public static int deathAge = 72;
-  public static int trainingYears = 3;
+  public static int trainingYears = 1;
   public static int birthAge = 30;
   public static int trainAge = 18;
-  public static int appPerProcess = 2;
+  public static int appPerProcess = 1;
 }
